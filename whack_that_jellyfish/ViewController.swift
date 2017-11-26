91e8c66121d4076860caed306ada94ce7b8b2346
Joe Hill <joe@jkh3.com>
2017-11-26 00:06:20 -0500

    My First Game in Augmented Reality

---
+++ whack_that_jellyfish/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  whack_that_jellyfish
+//
+//  Created by Joe Hill on 11/25/17.
+//  Copyright © 2017 Dumb Unicorn. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var SceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.SceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.SceneView.session.run(configuration)
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.SceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func play(_ sender: Any) {
+        self.addNode()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    func addNode() {
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.dataset/Jellyfish.scn")
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        jellyFishNode?.position = SCNVector3(0,0,-1)
+        self.SceneView.scene.rootNode.addChildNode(jellyFishNode!)
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            print("Didn't touch anything")
+        } else {
+            let results = hitTest.first!
+            let node = results.node
+            self.animateNode(node: node)
+        }
+    }
+    
+    func animateNode(node: SCNNode) {
+        let spin = CABasicAnimation(keyPath: "position")
+        spin.fromValue = node.presentation.position
+        spin.toValue = SCNVector3(0,0,-2)
+        node.addAnimation(spin, forKey: "position")
+    }
+    
+}
+
